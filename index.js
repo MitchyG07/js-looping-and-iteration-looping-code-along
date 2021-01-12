afebf5a1b763a512af986b5ffd06ac108537f974
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-01-11 16:28:46 -0800

    commit tree e60d34369ce09bdde89cc5c47a228558938cc37c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const cards = ["Ada", "Brendan", "Ali"]; 
+
+function writeCards(array, event) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+    }
+    return array
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);number--;
+    }
+} 
